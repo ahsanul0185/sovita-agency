11c9f067f3e28bd4bac21490865a9b8ca90c736a
Ahsanul Haque <ahsanul0185@gmail.com>
2025-11-03 15:55:15 +0600

    title text should appear other pages

--- app/get-started/layout.js
+++ app/get-started/layout.js
@@ -1,11 +1,17 @@
+"use client"
+
 import Breadcrumb from "@/component/get-started/Breadcrumb";
+import { usePathname } from "next/navigation";
 
 
 export default function GetStartedLayout({ children }) {
+
+   const pathname = usePathname();
+   const hideContactText = pathname.split("/")[pathname.split("/").length - 1] !== "get-started"
   return (
     <div className="p-6">
       <Breadcrumb /> {/* will appear on /get-started and all nested routes */}
-      <div className="text-center px-4 py-2 border border-gray-200/40 rounded w-fit mx-auto mt-6 mb-12 text-main-blue">Contacter notre équipe de 9h à 18h au 07 44 31 33 49 (appel gratuit)</div>
+      {hideContactText && <div className="text-center px-4 py-2 border border-gray-200/40 rounded w-fit mx-auto mt-6 mb-12 text-main-blue">Contacter notre équipe de 9h à 18h au 07 44 31 33 49 (appel gratuit)</div>}
       {children}    {/* renders the actual page content */}
     </div>
   );
