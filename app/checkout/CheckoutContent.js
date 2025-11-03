9e745279b3f3b9fe097c51f0eb2458fe18f48e36
Ahsanul Haque <ahsanul0185@gmail.com>
2025-11-03 21:14:32 +0600

    added allow in iframe

--- app/checkout/CheckoutContent.js
+++ app/checkout/CheckoutContent.js
@@ -24,6 +24,7 @@ export default function CheckoutContent() {
 
       <iframe
         style={{ width: "100%", height: "100%" }}
+        allow="payment"
         src={`https://billing.zohosecure.eu/subscribe/${plan}`}
       ></iframe>
     </div>
