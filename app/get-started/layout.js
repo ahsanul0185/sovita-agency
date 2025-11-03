0ff374027ad3034b0d10aecd917576c5d87e2615
Ahsanul Haque <ahsanul0185@gmail.com>
2025-11-03 20:43:49 +0600

    updated contact show text routes

--- app/get-started/layout.js
+++ app/get-started/layout.js
@@ -7,11 +7,25 @@ import { usePathname } from "next/navigation";
 export default function GetStartedLayout({ children }) {
 
    const pathname = usePathname();
-   const hideContactText = pathname.split("/")[pathname.split("/").length - 1] !== "get-started"
+  const showContactRoutes = ["vitrine", "ecommerce"];
+
+  // Check if the current path includes any of the specified routes
+  const showContactText = showContactRoutes.some(route =>
+    pathname.includes(route)
+  );
   return (
     <div className="p-6">
       <Breadcrumb /> {/* will appear on /get-started and all nested routes */}
-      {hideContactText && <div className="text-center px-4 py-2 border border-gray-200/40 rounded w-fit mx-auto mt-6 mb-12 text-main-blue">Contacter notre équipe de 9h à 18h au 07 44 31 33 49 (appel gratuit)</div>}
+      {showContactText && <div className="text-center px-6 py-4 border border-white/20 rounded w-fit mx-auto my-12">
+      
+                           
+      
+       <h2 className="mb-2">Une question sur votre commande ?</h2>
+        <p className="text-main-blue">
+          Contacter notre équipe de 9h à 18h au 07 44 31 33 49 (appel gratuit)
+        </p>
+      
+      </div>}
       {children}    {/* renders the actual page content */}
     </div>
   );
